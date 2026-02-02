8e64a2168c25647ef92a5e811485b23afe258e84
papangkorn00 <papangkorn.poomm@gmail.com>
2026-02-02 22:34:58 +0700

    style: font desc.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,26 +9,16 @@ const Home = () => {
 
       <div className="bg-base min-h-screen w-screen flex justify-center items-center">
         <div className="flex flex-col lg:flex-row items-center justify-center">
-          {/*Poom*/}
-          <div className="text-header w-3/4 md:w-2/5 lg:w-1/3">
-            {/* <h1 className="text-2xl font-extrabold font-media tracking-wider  lg:text-6xl">
-              PAPANGKORN
-            </h1>
-            <h2 className="text-xl font-bold font-media tracking-wide  lg:text-5xl">
-              KiJSAKULRAT😉
-            </h2> */}
-
+          <div className="text-header w-[64%] md:w-2/3 lg:w-[30%]">
             <h1 className="text-xl font-bold font-media tracking-wider md:text-4xl">
               Hi, I&apos;m Poom! 😉
             </h1>
-            <h2 className="text-xl md:text-2xl font-medium font-media tracking-wider text-wrap">
+            <h2 className="text-xs lg:text-xl font-medium font-media tracking-wider text-wrap">
               Developer who loves building websites and mastering new
               technologies.
             </h2>
           </div>
-          {/* Poom*/}
 
-          {/* Poom's image */}
           <div className="flex justify-center">
             <img
               src={PoomImgHome}
@@ -36,7 +26,6 @@ const Home = () => {
               className="max-w-[65%] lg:max-w-full lg:max-h-[100%]"
             />
           </div>
-          {/* Poom's image */}
         </div>
       </div>
 
