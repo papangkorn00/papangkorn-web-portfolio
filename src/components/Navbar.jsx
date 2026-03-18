64070386963cf3ef191d72d6af0edfcb76105815
papangkorn00 <papangkorn.poomm@gmail.com>
2026-03-19 01:10:43 +0700

    feat: card with corousel ez

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <Disclosure as="nav">
       <div className="fixed z-50 w-full mx-auto bg-black rounded-b-xl  px-2 sm:px-6 lg:px-10 ">
-        <div className="hidden lg:flex lg:h-24 lg:items-center lg:justify-between ">
+        <div className="flex lg:h-24 lg:items-center lg:justify-between ">
           <div className="flex space-x-5">
             {navigation.map((item) => (
               <NavLink
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,24 @@
 import Navbar from "./Navbar"
 import SocialLinks from "./SocialLinks"
 import Card from "./Ui/Card"
+import data from "../data/data.json"
 
 const Portfolio = () => {
   return (
     <div>
       <Navbar />
-      <div className="bg-base min-h-screen w-screen flex justify-center items-center">
-        <div className="w-3/4 lg:mt-40 flex flex-col lg:flex-col gap-y-5 items-center justify-center">
-          <Card />
-          <Card />
+
+      <div className="bg-base min-h-screen w-screen">
+        <div className="w-4/5 mx-auto py-20 lg:py-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.map((item) => (
+            <Card key={item.id} data={item} />
+          ))}
         </div>
       </div>
 
-      <SocialLinks />
+      <div className="hidden lg:flex">
+        <SocialLinks />
+      </div>
     </div>
   )
 }
